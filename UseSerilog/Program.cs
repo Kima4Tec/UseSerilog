22219d76f9c1c8f4b826caf05d8a4ae49d52533f
Kima4Tec <150744968+Kima4Tec@users.noreply.github.com>
2024-06-03 12:28:47 +0200

    Add project files.

---
+++ UseSerilog/Program.cs
@@ -0,0 +1,23 @@
+﻿using Serilog;
+using Serilog.Sinks;
+
+namespace UseSerilog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            Log.Information("Hello, Serilog!");
+
+            Log.Information("This is an informational message.");
+            Log.Warning("This is a warning message.");
+            Log.Error("This is an error message.");
+
+            Log.CloseAndFlush();
+        }
+    }
+}
